5b8121e384d4cc0436512a7d2bc9a1740903f55c
Rhoward1 <ryanmhoward@gmail.com>
2022-05-02 13:40:29 -0500

    handlebars view page working

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-  res.render('main')
-
+  res.render('home')
+  // res.sendFile(path.join(__dirname, '../views/layouts/main.handlebars'));
 })
-module.exports= router
+module.exports = router
