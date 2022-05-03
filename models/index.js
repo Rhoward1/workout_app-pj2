18c13c972d23d2248bbcaecb9e579ee170ca2d28
Sarah Turner <turnersarah228@gmail.com>
2022-05-03 16:34:28 -0500

    cleaned up models index.js file

--- models/index.js
+++ models/index.js
@@ -1,8 +1,5 @@
 const Member = require('./member');
 const PastWorkout = require('./pastWorkout')
-// const Workout_log = require("./workoutLog")
-// const CompWorkout = require('./compWorkout');
-// const CompWorkout = require('./compWorkout')
 
 Member.hasMany(PastWorkout, {
     foreignKey: 'member_id',
@@ -13,8 +10,5 @@ PastWorkout.belongsTo(Member, {
     foreignKey: 'member_id',
 })
 
-
-
 module.exports = { Member, PastWorkout }
-    // Workout_log}
-// CompWorkout };
+
