f4ef670270ec6b3e53ff9f4f6d9b675674f08ebc
jadehuynh <jadehuynh08@gmail.com>
2022-05-05 12:44:25 -0700

    somewhat logout feature

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  // store: new SequelizeStore({
-  //   db: sequelize,
-  // }),
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+
 // const hbs = exphbs.create({ });
 
 app.engine('handlebars', hbs.engine);
